2d9be12ef3aadc8ff230c0cb0ac74e296c68b4dd
shullmb <shullmb@gmail.com>
2018-07-09 19:43:07 -0700

    commit prior to conversion to mongo

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready( function() {
+    console.log('working');
+    // PUT route
+    $('#edit').on('submit', function(e) {
+        e.preventDefault();
+        var newData = $(this).serialize();
+        var url = $(this).attr('action');
+        $.ajax({
+            method: 'PUT',
+            url: url,
+            data: newData
+        }).done( function(data) {
+            console.log(data);
+            window.location = '/bikes';
+        })
+    })
+
+    // DELETE route
+    $('a').on('click', function(e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        $.ajax({
+            method: 'DELETE',
+            url: url
+        }).done( function(data) {
+            console.log(data);
+            window.location = '/bikes';
+        })
+    })
+})
+
